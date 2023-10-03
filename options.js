fd32457f3a58cac4a05d4e4f8c33ef0c605b0b45
Erica <ericabreig@gmail.com>
2023-10-02 20:56:54 -0400

    added js file

---
+++ options.js
@@ -0,0 +1,18 @@
+const outfits = [
+    'slutty teen',
+    'goth queen',
+    'witch bitch',
+    'studious college kid',
+    'athleisure MILF',
+    'bisexual icon',
+    'lesbian love interest from an indie film',
+    'school girl',
+    'some punk',
+    'boyfriend jeans',
+    'millennial disaster',
+    'sophisticated slut',
+    'functioning adult',
+    '1940s PinUp',
+    '1950s PinUp',
+
+];
